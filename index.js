96a6f412957ac21309618a8b1f12f2e23c7d2e74
Aman Ahamed <75929022+Aman235-code@users.noreply.github.com>
2024-06-03 21:54:47 +0530

    Update index.js

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ dotenv.config();
 app.use(express.json());
 app.use(cookieParser());
 app.use("/images", express.static(path.join(__dirname, "/images")));
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(cors({ origin: "https://code-tales-blog.vercel.app", credentials: true }));
 app.use("/api/auth", authRouter);
 app.use("/api/user", userRouter);
 app.use("/api/post", postRouter);
